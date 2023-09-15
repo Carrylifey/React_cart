9772089ce2aca023df080503dc1dd71b6bdf9313
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-09-16 01:35:21 +0530

    props

--- src/Cart.js
+++ src/Cart.js
@@ -3,12 +3,49 @@ import CartItem from './CartItem';
 
 
 class Cart extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            products:[
+                {
+                    price:99,
+                    title:"Watch",
+                    qty:1,
+                    img:'',
+                    id:1
+                  },
+                  {
+                    price:999,
+                    title:"Mobile phone",
+                    qty:10,
+                    img:'',
+                    id:2
+                  },
+                  {
+                    price:9999,
+                    title:"Laptop",
+                    qty:1,
+                    img:'',
+                    id:3
+                  }
+            ]
+          
+        }
+        // this.increaseQty=this.increaseQty.bind(this);
+      }
+    
     render() {
+        const{products}=this.state;
         return(
             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {products.map((product)=>{
+                    
+                    return <CartItem 
+                    product={product}  
+                     key={product.id} />
+
+                })}
+                
             </div>
         );
     }
