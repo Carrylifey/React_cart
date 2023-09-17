3a1fc30bad500cbd1f1bed8ccca47d9686581cee
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-09-17 06:28:23 +0530

    add total price

--- src/App.js
+++ src/App.js
@@ -1,12 +1,112 @@
 import React from 'react';
 import Cart from'./Cart';
+import Navbar from './Navbar';
 
-function App() {
+class  App extends React.Component { 
+  constructor(){
+      super()
+      this.state={
+          products:[
+              {
+                  price:99,
+                  title:"Watch",
+                  qty:1,
+                  img:'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80',
+                  id:1
+                },
+                {
+                  price:999,
+                  title:"Mobile phone",
+                  qty:10,
+                  img:'https://images.unsplash.com/photo-1585060544812-6b45742d762f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2081&q=80',
+                  id:2
+                },
+                {
+                  price:9999,
+                  title:"Laptop",
+                  qty:1,
+                  img:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
+                  id:3
+                }
+          ]
+        
+      }
+      // this.increaseQty=this.increaseQty.bind(this);
+    }
+    handleIncreaseQuantity= (product) =>{
+      console.log('hey increase ',product);
+      const{products}=this.state;
+      const index = products.indexOf(product);
+
+      products[index].qty+=1;
+      this.setState({
+        products
+      })
+    }
+
+    handleDecreaseQuantity= (product) =>{
+      console.log('hey increase ',product);
+      const{products}=this.state;
+      const index = products.indexOf(product);
+
+      if (products[index].qty===0){
+        return;
+      }
+      products[index].qty-=1;
+      this.setState({
+        products
+      })
+    }
+  
+    handleDeleteProduct=(id)=>{
+      const {products}=this.state;
+
+      const items=products.filter((item)=>item.id!==id);
+
+      this.setState({
+        products:items
+      })
+    }
+    GetCartCount =() =>{
+      const {products}= this.state
+
+
+        let count=0;
+        products.forEach((product) => {
+            count += product.qty
+        });
+
+        return count;
+    }
+    GetCartTotal=() =>{
+      const {products}= this.state
+
+      let cartTotal =0;
+
+      products.map((product)=>{
+        cartTotal = cartTotal+product.qty*product.price
+
+      })
+      return cartTotal;
+    }
+  
+  render(){
+    const{products}=this.state
   return (
     <div className="App">
-      <Cart />
+      <Navbar count={this.GetCartCount()}/>
+      <Cart 
+      products={products}
+      onIncreaseQuantity={this.handleIncreaseQuantity}
+      onDecreaseQuantity={this.handleDecreaseQuantity}
+      onDeleteProduct={this.handleDeleteProduct}
+      />
+      <div>
+        Total : {this.GetCartTotal()}
+      </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,54 +1,53 @@
-import React from 'react';
+import React from "react";
 
-class CartItem extends React.Component {
-  
+const CartItem = (props) => {
+  // console.log('this.props',this.props)
+  const { price, title, qty } = props.product;
+  const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } =
+    props;
 
-  render () {
-    console.log('this.props',this.props)
-    const{price,title,qty}=this.props.product;
-    const{product,
-      onIncreaseQuantity,
-      onDecreaseQuantity,
-      onDeleteProduct}=this.props;
-
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img alt="" style ={styles.image} />
-        </div>
-        <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price}</div>
-          <div style={ { color: '#777' } }>Qty: {qty}</div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img alt="increase" className='action-icons' 
-              src='https://cdn-icons-png.flaticon.com/128/4315/4315609.png'
-              onClick={()=>onIncreaseQuantity(product)}
-              />
-            <img alt="decrease" className='action-icons' 
-              src='https://cdn-icons-png.flaticon.com/128/4315/4315581.png'
-              onClick={()=>onDecreaseQuantity(product)}
-              />
-            <img alt="delete" className='action-icons' 
-            src='https://cdn-icons-png.flaticon.com/128/1828/1828843.png'
-              onClick={()=>onDeleteProduct(product.id)}
-            />
-          </div>
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img alt="" style={styles.image} src={product.img} />
+      </div>
+      <div className="right-block">
+        <div style={{ fontSize: 25 }}>{title}</div>
+        <div style={{ color: "#777" }}>Rs {price}</div>
+        <div style={{ color: "#777" }}>Qty: {qty}</div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/128/4315/4315609.png"
+            onClick={() => onIncreaseQuantity(product)}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/128/4315/4315581.png"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/128/1828/1828843.png"
+            onClick={() => onDeleteProduct(product.id)}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const styles = {
   image: {
     height: 120,
     width: 110,
     borderRadius: 5,
-    background: 'grey'
-    
-  }
-}
+    background: "grey",
+  },
+};
 
 export default CartItem;
